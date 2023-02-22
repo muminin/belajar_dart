483ba975fcc8d3a802347f60bbbd97c6adcd8d31
Amirul Mu'minin <muminin71@gmail.com>
2023-02-23 05:26:23 +0700

    007 Inheritance

--- bin/007_inheritance.dart
+++ bin/007_inheritance.dart
@@ -4,23 +4,43 @@ import 'package:test_dart/monster_ikan.dart';
 import 'package:test_dart/monster_kecoa.dart';
 
 void main(List<String> args) {
+  // Hero heroA = Hero();
+  // // Monster monsterA = Monster();
+  // MonsterIkan monsterIkan = MonsterIkan();
+  // MonsterKecoa monsterKecoa = MonsterKecoa();
+
+  // heroA.health = 10;
+  // // monsterA.health = 20;
+  // monsterIkan.health = 20;
+  // monsterKecoa.health = 100;
+
+  // print("Hero HP: ${heroA.health.toString()}");
+  // print(heroA.killMonster());
+
+  // // print("Monster HP: ${monsterA.health.toString()}");
+  // // print(monsterA.eatHuman());
+
+  // print("Monster Ikan HP: ${monsterIkan.health.toString()}");
+  // print("Monster Ikan ${monsterIkan.eatHuman()}");
+  // print(monsterIkan.swim());
+
   Hero heroA = Hero();
-  // Monster monsterA = Monster();
+  Monster monsterA = Monster();
   MonsterIkan monsterIkan = MonsterIkan();
-  MonsterKecoa monsterKecoa = MonsterKecoa();
 
-  heroA.health = 10;
-  // monsterA.health = 20;
-  monsterIkan.health = 20;
-  monsterKecoa.health = 100;
+  Monster monsterKecoa = MonsterKecoa();
+  print((monsterKecoa as MonsterKecoa).fly());
 
-  print("Hero HP: ${heroA.health.toString()}");
-  print(heroA.killMonster());
+  List<Monster> monsters = [];
 
-  // print("Monster HP: ${monsterA.health.toString()}");
-  // print(monsterA.eatHuman());
+  monsters.add(MonsterIkan());
+  monsters.add(MonsterKecoa());
+  monsters.add(MonsterKecoa());
 
-  print("Monster Ikan HP: ${monsterIkan.health.toString()}");
-  print(monsterIkan.swim());
-  print("Monster Ikan ${monsterIkan.eatHuman()}");
+  for (Monster mon in monsters) {
+    print(mon.eatHuman());
+    if (mon is MonsterKecoa) {
+      print(mon.fly());
+    }
+  }
 }
